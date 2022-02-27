112c21240bc64cb6428e6c9c8d3fe91a6096e805
Nachiappan N K <nachiapan@gmail.com>
2022-02-27 13:46:33 +0530

    Attempted logging to application insights through serilog

--- azurefn/azurefn/Program.cs
+++ azurefn/azurefn/Program.cs
@@ -1,6 +1,11 @@
+using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.Extensibility;
 using Microsoft.Azure.Functions.Worker.Configuration;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Serilog;
+using System;
 using System.Threading.Tasks;
 
 namespace azurefn
@@ -11,9 +16,27 @@ namespace azurefn
         {
             var host = new HostBuilder()
                 .ConfigureFunctionsWorkerDefaults()
+                .ConfigureServices(services => {
+                    TelemetryConfiguration telemetryConfig = TelemetryConfiguration.CreateDefault();
+                    telemetryConfig.InstrumentationKey = GetEnvironmentVariable("InstrumentationKey");
+                    TelemetryClient telemetryClient = new TelemetryClient(telemetryConfig);
+                    var logger = new LoggerConfiguration()
+                        .Enrich.WithProperty("applicationName", "FirstAzureFn")
+                        .Enrich.WithProperty("Location", "USEast")
+                   .WriteTo.Console()
+                   .WriteTo.ApplicationInsights(telemetryClient, TelemetryConverter.Traces)
+                   .CreateLogger();
+                    services.AddLogging(lb => lb.AddSerilog(logger));
+                    services.AddSingleton<TelemetryClient>(telemetryClient);
+                })
                 .Build();
 
             host.Run();
+        }
+        private static string GetEnvironmentVariable(string name)
+        {
+            return name + ": " +
+                System.Environment.GetEnvironmentVariable(name, EnvironmentVariableTarget.Process);
         }
     }
 }
