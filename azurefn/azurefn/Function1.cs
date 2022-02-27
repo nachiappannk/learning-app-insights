2e7ed99a3c2836333eb9252e86c105055a4e4799
Nachiappan N K <nachiapan@gmail.com>
2022-02-27 14:24:37 +0530

    Checking dependencies

--- azurefn/azurefn/Function1.cs
+++ azurefn/azurefn/Function1.cs
@@ -1,24 +1,29 @@
-using System.Collections.Generic;
-using System.Net;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace azurefn
 {
     public static class Function1
     {
         [Function("Function1")]
-        public static HttpResponseData Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req,
+        public static async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req,
             FunctionContext executionContext)
         {
             var logger = executionContext.GetLogger("Function1");
             logger.LogInformation("C# HTTP trigger function processed a request. log");
 
+            HttpClient client = new HttpClient();
+            var response1 = await client.GetAsync("https://google.com/");
+            
+
             var response = req.CreateResponse(HttpStatusCode.OK);
             response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
 
-            response.WriteString("Welcome to Azure Functions!");
+            response.WriteString($"Welcome to Azure Functions! {response1.StatusCode}");
 
             return response;
         }
