7b1b173c52a4b811607a0dd8553ad22cfeec0d39
Nachiappan N K <nachiapan@gmail.com>
2022-03-16 23:22:06 +0530

    Working dependency

--- azurefn/azurefn1/Function1.cs
+++ azurefn/azurefn1/Function1.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
+using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.DataContracts;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
@@ -13,10 +15,12 @@ namespace azurefn1
     public class Function1
     {
         private readonly IHttpClientFactory clientFactory;
+        private readonly TelemetryClient telemetryClient;
 
-        public Function1(Test client, IHttpClientFactory clientFactory)
+        public Function1(Test client, IHttpClientFactory clientFactory, TelemetryClient telemetryClient)
         {
             this.clientFactory = clientFactory;
+            this.telemetryClient = telemetryClient;
         }
 
         public static string RandomString(int length)
@@ -47,6 +51,31 @@ namespace azurefn1
             logger.LogWarning($"## executionContext.RetryContext.RetryCount: {GetRetryCount(executionContext)}");
             logger.LogWarning($"## executionContext.TraceContext.TraceParent: {executionContext.TraceContext.TraceParent}");
             logger.LogWarning($"## executionContext.TraceContext.TraceState: {executionContext.TraceContext.TraceState}");
+
+            logger.LogWarning($"## telemetryClient.Context.Operation.Id: {telemetryClient.Context.Operation.Id}");
+            logger.LogWarning($"## telemetryClient.Context.Operation.ParentId: {telemetryClient.Context.Operation.ParentId}");
+            logger.LogWarning($"## telemetryClient.Context.Operation.Name: {telemetryClient.Context.Operation.Name}");
+
+
+            DependencyTelemetry telemetry = new DependencyTelemetry();
+            telemetry.Context.Operation.Id = $"nachi-{RandomString(16)}";
+            telemetry.Context.Operation.ParentId = $"nachi-parent-{RandomString(16)}";
+            telemetry.Context.Operation.Name = $"nachi-name-{RandomString(16)}";
+            telemetry.Id = $"nachi-telemetyid-{RandomString(16)}";
+            telemetry.Properties.Add("one", "tow");
+            telemetry.Data = "calling http something";
+            telemetry.ResultCode = "sucess";
+            telemetry.Success = true;
+            telemetry.Type = "HTTP";
+            telemetry.Duration = new TimeSpan(0, 0, 0, 0, 200);
+            telemetry.Name = "name-nachi";
+            telemetry.Target = "dflkajs-target";
+            telemetry.Timestamp = DateTime.UtcNow;
+
+
+
+            telemetryClient.TrackDependency(telemetry);
+ 
 
             try
             {
--- azurefn/azurefn1/Program.cs
+++ azurefn/azurefn1/Program.cs
@@ -6,6 +6,9 @@ using Microsoft.ApplicationInsights.Extensibility;
 using Microsoft.ApplicationInsights.Channel;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.ApplicationInsights;
+using System;
+using Microsoft.ApplicationInsights.DataContracts;
 
 namespace azurefn1
 {
@@ -18,19 +21,56 @@ namespace azurefn1
                 .ConfigureLogging((context, builder) => {
                     builder.AddApplicationInsights("2811a959-1208-4f03-8ef3-223560281d0e");
                 })
-                .ConfigureServices( x => {
-                    x
+                .ConfigureServices( serviceProvider =>
+                {
+                    serviceProvider
                         .AddHttpClient()
                         .AddLogging()
-                        .AddSingleton<Test>();
+                        .AddSingleton<Test>()
+                        .AddSingleton(s => GetTelemetryClient());//check scoped
+                    ;
                 })
                 .Build();
 
             host.Run();
         }
+
+        private static TelemetryClient GetTelemetryClient()
+        {
+            TelemetryConfiguration telemetryConfig = TelemetryConfiguration.CreateDefault();
+            telemetryConfig.TelemetryInitializers.Add(new CloudRoleNameTelemetryInitializer());
+            telemetryConfig.InstrumentationKey = GetEnvironmentVariable("InstrumentationKey");
+            TelemetryClient telemetryClient = new TelemetryClient(telemetryConfig);
+            return telemetryClient;
+        }
+
+        private static string GetEnvironmentVariable(string name)
+        {
+            return Environment.GetEnvironmentVariable(name, EnvironmentVariableTarget.Process);
+        }
     }
 
     public class Test 
     { 
+    }
+
+
+    public class CloudRoleNameTelemetryInitializer : ITelemetryInitializer
+    {
+
+        public CloudRoleNameTelemetryInitializer()
+        {
+            
+        }
+
+        public void Initialize(ITelemetry telemetry)
+        {
+            // set custom role name here
+            telemetry.Context.Cloud.RoleName = "newnachiazurefn";
+            var requestTelemetry = telemetry as DependencyTelemetry;
+            if (requestTelemetry == null) return;
+            requestTelemetry.Properties.Add("LoggedInUserName", "DummyUser");
+
+        }
     }
 }
